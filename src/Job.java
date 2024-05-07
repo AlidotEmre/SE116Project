2caa52bc127d0f87c06650489e63bbd6abd38c59
AlidotEmre <aliemre3443@gmail.com>
2024-05-07 21:58:49 +0300

    version 2

---
+++ src/Job.java
@@ -0,0 +1,33 @@
+public abstract class Job {
+    private String jobId;
+    private String jobType;
+    private int startTime;
+    private int duration;
+
+    public Job(String jobId, String jobType, int startTime, int duration) {
+        this.jobId = jobId;
+        this.jobType = jobType;
+        this.startTime = startTime;
+        this.duration = duration;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    // Abstract methods that must be implemented by subclasses
+    public abstract void startNextTask();
+    public abstract void updateJobStatus();
+}
